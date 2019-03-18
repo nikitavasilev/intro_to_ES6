0d335bb878703859caa0564c037d4403c8ab5aee
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-03-19 00:49:22 +0100

    es6 strings + concat

---
+++ strings.js
@@ -0,0 +1,26 @@
+// Strings in ES6
+
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year) {
+  return 2016 - year;
+}
+
+// ES5
+console.log('This is ' + firstName + ' ' + lastName +
+'. He was born in ' + yearOfBirth + '. Today, he is ' +
+calcAge(yearOfBirth) + ' years old.');
+
+// ES6
+console.log(`This is ${firstName} \
+${lastName}. He was born in \
+${yearOfBirth}. Today, he is \
+${calcAge(yearOfBirth)} years old.`);
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('j'));
+console.log(n.endsWith('h'));
+console.log(n.includes(' '));
+console.log(firstName.repeat(5));
